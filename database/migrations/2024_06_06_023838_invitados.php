fffb67ddb3b3558d6c92d873409bad1c322f078c
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-06-05 20:48:21 -0600

    Release: Se agrega archivo de rutas para API

---
+++ database/migrations/2024_06_06_023838_invitados.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_invitado', 255)->default('');
+            $table->unsignedTinyInteger('numero_invitados')->default(1);
+            $table->string('token_invitado')->default('');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitados');
+    }
+};
