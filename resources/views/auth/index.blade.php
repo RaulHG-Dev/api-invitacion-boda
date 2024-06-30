896eb98f2ea20ee87f61c002aaf08cc135051137
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-06-30 14:13:54 -0600

    Fix: Se corrige title de login

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>S&A | Panel Administrador</title>
+    <title>S&E | Panel Administrador</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-[url('/images/background.webp')] bg-cover bg-no-repeat w-max h-max bg-center">
