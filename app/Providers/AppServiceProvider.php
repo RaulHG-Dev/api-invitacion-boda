0dd7b38457c07c744826d2afb3b1afc7442ae3c7
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2026-07-18 12:31:24 -0600

    fix: https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (! app()->runningInConsole() && str_contains(request()->header('x-forwarded-proto', ''), 'https')) {
             URL::forceScheme('https');
         }
     }
